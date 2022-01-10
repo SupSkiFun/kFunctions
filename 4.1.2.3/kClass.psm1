59958db6d74a80a953dc8aec53fa233af338ee36
SupSkiFun <SupSkiFun@gmail.com>
2022-01-09 19:44:10 -0700

    stubbed new release

---
+++ 4.1.2.3/kClass.psm1
@@ -0,0 +1,119 @@
+class K8sAPI
+{
+    static $uric = '/api/v1/'
+    static $uria = '/apis/'
+    static $urip = '/api/v1/pods'
+    static $urin = '/api/v1/namespaces/'
+    static $mesg = "Terminating.  Non-valid URL detected.  Submitted URL:  "
+
+    static [boolean] CheckURI ([uri] $uri)
+    {
+        return ([uri] $uri).IsAbsoluteUri
+    }
+
+    static [psobject] GetApiInfo ( [string] $mainurl )
+    {
+        $mainurl
+        $apis =  Invoke-RestMethod -Method Get -Uri $mainurl
+        return $apis
+    }
+
+    static [psobject] GetResourceInfo ( [string] $url )
+    {
+        $resq = Invoke-RestMethod -Method Get -Uri $url
+        $resi = $resq.resources.Where({$_.name -notmatch "/"})
+        return $resi
+    }
+
+    static [pscustomobject] MakeContainerObj ([psobject] $cont)
+    {
+        $arr = [System.Collections.ArrayList]::new()
+        foreach ($c in $cont)
+        {
+            $lo = [PSCustomObject]@{
+                Name = $c.name
+                Image = $c.image
+                VolumeMounts = $c.VolumeMounts
+                ImagePullPolicy = $c.ImagePullPolicy
+                Environment = $c.env
+            }
+            $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.Kubernetes.Container.Info')
+            $arr.add($lo)
+        }
+        return $arr
+    }
+
+    static [pscustomobject] MakeNameSpaceObj ([psobject] $ns)
+    {
+        $lo = [PSCustomObject]@{
+            Name = $ns.metadata.name
+            Creation = $ns.metadata.creationTimestamp
+            SelfLink = $ns.metadata.selfLink
+            Status = $ns.status.phase
+        }
+        $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.Kubernetes.NameSpace.Info')
+        return $lo
+    }
+
+    static [pscustomobject] MakeObj (
+            [string] $nom ,
+            [string] $grv ,
+            [psobject] $res
+        )
+    {
+        $lo = [PSCustomObject]@{
+            GroupName = "core"
+            GroupVersion = $grv
+            Version = $grv
+            PreferredVersion = $true
+            ResourceName = $res.name
+            ResourceKind = $res.kind
+            ShortName = $res.shortNames
+            NameSpaced = $res.namespaced
+            Verbs = $res.verbs
+        }
+        $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.Kubernetes.API.Info')
+        return $lo
+    }
+
+    static [pscustomobject] MakeObj (
+            [string] $nom ,
+            [psobject] $grv ,
+            [psobject] $res ,
+            [string] $prv
+        )
+    {
+        $gvv = $grv.groupVersion
+
+        $lo = [PSCustomObject]@{
+            GroupName = $nom
+            GroupVersion = $gvv
+            Version = $grv.version
+            PreferredVersion = if ($prv -eq $gvv) {$true} else {$false}
+            ResourceName = $res.name
+            ResourceKind = $res.kind
+            ShortName = $res.shortNames
+            NameSpaced = $res.namespaced
+            Verbs = $res.verbs
+        }
+        $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.Kubernetes.API.Info')
+        return $lo
+    }
+
+    static [pscustomobject] MakePodObj ([psobject] $pod)
+    {
+        $cs = [K8sAPI]::MakeContainerObj($pod.spec.containers)
+        $lo = [PSCustomObject]@{
+            Name = $pod.metadata.name
+            NameSpace = $pod.metadata.namespace
+            NodeName = $pod.spec.nodeName
+            Status = $pod.status.phase
+            Creation = $pod.metadata.creationTimestamp
+            SelfLink = $pod.metadata.selfLink
+            Labels = $pod.metadata.Labels
+            Containers = $cs
+        }
+        $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.Kubernetes.Pod.Info')
+        return $lo
+    }
+}
---
+++ 4.1.2.3/kFunctions.psm1
@@ -0,0 +1,269 @@
+using module .\kClass.psm1
+
+<#
+.SYNOPSIS
+Produces an object of Kubernetes API Groups and Resources.
+.DESCRIPTION
+Produces an object of Kubernetes API Groups and Resources via proxied connection.
+Combines the output of (kubectl api-resources) and (kubectl api-versions).
+See Notes and Examples.
+.PARAMETER Uri
+URI that has been proxied via kubectl.
+.INPUTS
+URI that has been proxied via kubectl.
+.OUTPUTS
+pscustombobject SupSkiFun.Kubernetes.API.Info
+.NOTES
+1.  Command works both locally (Linux) and remotely (Linux or Windows).
+2.  For this Advanced Function to work properly:
+    a) Ensure that the API has been proxied:
+        Start-Job -ScriptBlock {kubectl proxy --port 8888}
+    b) Run the command, returning the information into a variable:
+        $myVar = Get-K8sAPIInfo -Uri http://127.0.0.1:8888
+3.  The DefaultDisplayPropertySet = "GroupName","GroupVersion","ResourceKind","ResourceName"
+    To see all properties, issue either:
+        $myVar | Format-List -Property *
+        $myVar | Select-Object -Property *
+4. If using microK8s it may be necessary to run 'microk8s kubectl' in place of 'kubectl'.
+5. For PowerShell 7, ensure the use of 127.0.0.1 instead of localhost.  Using localhost is far slower.
+.EXAMPLE
+Please Read:
+
+Note: Any free port above 1024 can be used; if using a port different than 8888, substitute accordingly.
+Note: If using microK8s it may be necessary to run 'microk8s kubectl' in place of 'kubectl'.
+
+Before this Advanced Function will work, a proxy to the API must be configured.
+    Start-Job -ScriptBlock {kubectl proxy --port 8888}
+
+Once the proxy is established:
+    $myVar = Get-K8sAPIInfo -Uri http://127.0.0.1:8888
+
+Display the Default Property Set of all Groups / Resources:
+    $myVar
+
+Display all Properties of all Groups / Resources:
+    $myVar | Format-List -Property *
+
+Display all Preferred Version Groups / Resources:
+    $myVar | Where-Object -Property PreferredVersion -eq $true
+        or
+    $myVar | Where-Object -Property PreferredVersion -eq $true | fl *
+
+Display all Groups / Resources within the apps group:
+    $myVar | Where-Object -Property GroupName -eq apps
+        or
+    $myVar | Where-Object -Property GroupName -eq apps | fl *
+
+Display all Groups / Resources matching the ResourceKind Role:
+    $myVar | Where-Object -Property ResourceKind -match role
+        or
+    $myVar | Where-Object -Property ResourceKind -match role | fl *
+#>
+
+Function Get-K8sAPIInfo
+{
+    [cmdletbinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true , ValueFromPipeline = $true)]
+        [Uri] $Uri
+    )
+
+    Begin
+    {
+        if (-not([K8sAPI]::CheckUri($uri)))
+        {
+            Write-Output $(([K8sAPI]::mesg) + $uri)
+            break
+        }
+
+        $urla = ($($Uri.AbsoluteUri)+$([K8sAPI]::uria))
+        $urlc = ($($Uri.AbsoluteUri)+$([K8sAPI]::uric))
+    }
+
+    Process
+    {
+        $apic = [K8sAPI]::GetApiInfo($urlc)
+        $rr = $apic.resources |
+            Where-Object -Property Name -NotMatch "/"
+        foreach ($ap in $rr)
+        {
+            $lo = [K8sAPI]::MakeObj($apic.kind , $apic.groupVersion , $ap )
+            $lo
+        }
+
+        $apis = [K8sAPI]::GetApiInfo($urla)
+        foreach ($api in $apis.groups)
+        {
+            $prv = $api.preferredVersion.groupVersion
+            $grvs = $api.versions
+            foreach ($grv in $grvs)
+            {
+                $url = $($urla)+$($grv.groupVersion)
+                $resi = [K8sAPI]::GetResourceInfo($url)
+                foreach ($res in $resi)
+                {
+                    $lo = [K8sAPI]::MakeObj($api.name , $grv , $res , $prv)
+                    $lo
+                }
+            }
+        }
+    }
+
+    End
+    {
+        $TypeData = @{
+            TypeName = 'SupSkiFun.Kubernetes.API.Info'
+            DefaultDisplayPropertySet = "GroupName","GroupVersion","ResourceKind","ResourceName"
+        }
+        Update-TypeData @TypeData -Force
+    }
+}
+
+<#
+.SYNOPSIS
+Produces an object of Kubernetes NameSpaces.
+.DESCRIPTION
+Produces an object of Kubernetes Namespaces via proxied connection.
+See Notes and Examples.
+.PARAMETER Uri
+URI that has been proxied via kubectl.
+.INPUTS
+URI that has been proxied via kubectl.
+.OUTPUTS
+pscustombobject SupSkiFun.Kubernetes.NameSpace.Info
+.NOTES
+1.  Command works both locally (Linux) and remotely (Linux or Windows).
+2.  For this Advanced Function to work properly:
+    a) Ensure that the API has been proxied:
+        Start-Job -ScriptBlock {kubectl proxy --port 8888}
+    b) Run the command, returning the information into a variable:
+        $myVar = Get-K8sNamespace -Uri http://127.0.0.1:8888
+3. If using microK8s it may be necessary to run 'microk8s kubectl' in place of 'kubectl'.
+4. For PowerShell 7, ensure the use of 127.0.0.1 instead of localhost.  Using localhost is far slower.
+.EXAMPLE
+Please Read:
+
+Note: Any free port above 1024 can be used; if using a port different than 8888, substitute accordingly.
+Note: If using microK8s it may be necessary to run 'microk8s kubectl' in place of 'kubectl'.
+
+Before this Advanced Function will work, a proxy to the API must be configured.
+    Start-Job -ScriptBlock {kubectl proxy --port 8888}
+
+Once the proxy is established:
+    Get-K8sNamespace -Uri http://127.0.0.1:8888
+#>
+
+Function Get-K8sNamespace
+{
+    [cmdletbinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true , ValueFromPipeline = $true)]
+        [Uri] $Uri
+    )
+
+    Begin
+    {
+        if (-not([K8sAPI]::CheckUri($uri)))
+        {
+            Write-Output $(([K8sAPI]::mesg) + $uri)
+            break
+        }
+
+        $urln = ($($Uri.AbsoluteUri)+$([K8sAPI]::urin))
+    }
+
+    Process
+    {
+        $apin = [K8sAPI]::GetApiInfo($urln)
+        foreach ($ns in $apin.items)
+        {
+            $lo = [K8sAPI]::MakeNameSpaceObj($ns)
+            $lo
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+Produces an object of Kubernetes Pods.
+.DESCRIPTION
+Produces an object of Pods and their container(s) via proxied connection.
+See Notes and Examples.
+.PARAMETER Uri
+URI that has been proxied via kubectl.
+.INPUTS
+URI that has been proxied via kubectl.
+.OUTPUTS
+pscustombobject SupSkiFun.Kubernetes.Pods.Info
+.NOTES
+1.  Command works both locally (Linux) and remotely (Linux or Windows).
+2.  For this Advanced Function to work properly:
+    a) Ensure that the API has been proxied:
+        Start-Job -ScriptBlock {kubectl proxy --port 8888}
+    b) Run the command, returning the information into a variable:
+        $myVar = Get-K8sPod -Uri http://127.0.0.1:8888
+3. If using microK8s it may be necessary to run 'microk8s kubectl' in place of 'kubectl'.
+4. For PowerShell 7, ensure the use of 127.0.0.1 instead of localhost.  Using localhost is far slower.
+.EXAMPLE
+Please Read:
+
+Note: Any free port above 1024 can be used; if using a port different than 8888, substitute accordingly.
+Note: If using microK8s it may be necessary to run 'microk8s kubectl' in place of 'kubectl'.
+
+Before this Advanced Function will work, a proxy to the API must be configured.
+    Start-Job -ScriptBlock {kubectl proxy --port 8888}
+
+Once the proxy is established:
+    $myVar = Get-K8sPod -Uri http://127.0.0.1:8888
+
+Display all pods:
+    $myVar
+
+Drill into one pod's labels:
+    ($myVar[0]).Labels
+
+Drill into one pod's container(s):
+    ($myVar[0]).Containers
+
+Drill into one pod's container's Environment:
+    ($myVar[0]).Containers[0].Environment
+
+Drill into one pod's container's Image:
+    ($myVar[0]).Containers[0].Image
+
+Convert the entire object to JSON:
+    $myVar | ConvertTo-Json -Depth 10
+#>
+
+Function Get-K8sPod
+{
+    [cmdletbinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true , ValueFromPipeline = $true)]
+        [Uri] $Uri
+    )
+
+    Begin
+    {
+        if (-not([K8sAPI]::CheckUri($uri)))
+        {
+            Write-Output $(([K8sAPI]::mesg) + $uri)
+            break
+        }
+
+        $urlp = ($($Uri.AbsoluteUri)+$([K8sAPI]::urip))
+    }
+
+    Process
+    {
+        $apip = [K8sAPI]::GetApiInfo($urlp)
+        foreach ($pod in $apip.items)
+        {
+            $lo = [K8sAPI]::MakePodObj($pod)
+            $lo
+        }
+    }
+}
